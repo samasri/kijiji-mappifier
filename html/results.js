0d7f6bf6e87f24a40a23262136832c95e900afff
Samer AL Masri <almasri@ualberta.ca>
2020-03-02 04:19:27 +0000

    Worked on UI
    Signed-off-by: Samer AL Masri <almasri@ualberta.ca>

--- html/results.js
+++ html/results.js
@@ -9,19 +9,57 @@ $(document).ready(() => {
         accessToken: token
     }).addTo(mymap);
 
+    var markers = {}
+    var displayedMarkers = []
+    let id = 0
     data.forEach(element => {
-        // alert("Title: " + element.title);
-        // alert("Price: " + element.price);
-        // alert("URL: " + element.url);
-        // alert("Address: " + element.address);
-
-        var displayedMarkers = L.featureGroup().addTo(mymap);
-        var marker = L.marker([element.lat, element.lon]).addTo(displayedMarkers);
+        var marker = L.marker([element.lat, element.lon]).addTo(mymap);
         var content = `
         <p> Title: ${element.title} </p>
         <p> Price: ${element.price} </p>
         <p> <a href="${element.link}">kijiji link</a> </p>
         `
         marker.bindPopup(content);
+        markers[id++] = marker;
+        displayedMarkers.push(id)
     });
+
+    mymap.on('moveend', function(e) {
+
+        // Get boundaries of new view
+        var bounds = mymap.getBounds();
+        var north = bounds.getNorth();
+        var south = bounds.getSouth();
+        var east = bounds.getEast();
+        var west = bounds.getWest();
+
+        var toShow = []
+        var toHide = []
+        Object.keys(markers).forEach((id) => {
+            let element = markers[id]
+            let lat = element.getLatLng().lat;
+            let lng = element.getLatLng().lng;
+            if(lat > north ||
+                lat < south ||
+                lng > east ||
+                lng < west) toHide.push(id)
+            else toShow.push(id)
+        });
+        console.log("Hiding: " + toHide.length);
+        console.log("Showing: " + toShow.length);
+        if(toShow.length > 1000) {
+            toShow = toShow.slice(1,1000);
+            console.log("Limiting to 2000 entries");
+        }
+        toHide.forEach(id => {
+            mymap.removeLayer(markers[id])
+        });
+        toShow.forEach(id => {
+            let marker = markers[id]
+            if(!mymap.hasLayer(marker)) mymap.addLayer(marker)
+        })
+
+        
+    });
+
 });
