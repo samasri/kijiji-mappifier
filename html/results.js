f37075f220017293754ddc78778eb3bd112943ce
Samer AL Masri <samasri@ibm.com>
2020-03-02 23:48:50 -0500

    Now all results are processed by the backend and price filter happens at the front end
    Signed-off-by: Samer AL Masri <samasri@ibm.com>

--- html/results.js
+++ html/results.js
@@ -1,3 +1,21 @@
+function displayMarkers(inp,clusterGroup,min=-1,max=-1) {
+    inp.forEach(element => {
+        var marker = L.marker([element.lat, element.lon]);
+        if((min == -1 || element.price >= min) && 
+            (max == -1 || element.price <= max)) {
+                clusterGroup.addLayer(marker);
+                var content = `
+                <p> <a href="${element.link}" target="_blank">${element.title}</a> </p>
+                <p> Price: ${element.price} </p>
+                `
+                marker.bindPopup(content);
+            }
+    });
+}
+
+var minPrice = -1;
+var maxPrice = -1;
+
 $(document).ready(() => {
     var mymap = L.map('mapid').setView([43.8524, -79.4162], 11);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -10,20 +28,22 @@ $(document).ready(() => {
     }).addTo(mymap);
 
     var markersClusterGroup = L.markerClusterGroup({disableClusteringAtZoom: 15});
-    var displayedMarkers = []
     let id = 0
-    data.forEach(element => {
-        var marker = L.marker([element.lat, element.lon]);
-        markersClusterGroup.addLayer(marker);
-        var content = `
-        <p> Title: ${element.title} </p>
-        <p> Price: ${element.price} </p>
-        <p> <a href="${element.link}">kijiji link</a> </p>
-        `
-        marker.bindPopup(content);
-        displayedMarkers.push(id)
-    });
+    displayMarkers(data,markersClusterGroup);
 
     mymap.addLayer(markersClusterGroup);
+
+    $("#minPrice").change(event => {
+        let value = event.currentTarget.value;
+        minPrice = value;
+        markersClusterGroup.clearLayers();
+        displayMarkers(data,markersClusterGroup,minPrice,maxPrice);
+    });
+    $("#maxPrice").change(event => {
+        let value = event.currentTarget.value;
+        maxPrice = value;
+        markersClusterGroup.clearLayers();
+        displayMarkers(data,markersClusterGroup,minPrice,maxPrice);
+    });
 
 });
