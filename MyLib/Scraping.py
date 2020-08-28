e8dbd5c91618fd9cf9e257964d92206979d49cbd
Samer Almasri <almasri@ualberta.ca>
2020-08-28 16:10:50 +0000

    Various Changes
    * Reorganized Code: changing library functions to serve more as a backend and main scraper.py to serve more as a front end
    ----------------------------------------------------------------------
    * Added waiting times on errors: Kijiji sometimes stops responding with meaningful webpages when multiple requests are sent at the same time. So I change the code to wait one minute when Kijiji stops responding as expected.
    * Changed way location is obtained: Using a more specific html class to obtain the location
    
    Signed-off-by: Samer Almasri <almasri@ualberta.ca>

--- MyLib/Scraping.py
+++ MyLib/Scraping.py
@@ -1,63 +1,81 @@
-import urllib.request
+import urllib.request, re, time
 from bs4 import BeautifulSoup
-import re
 
-def __getAdAddress(link):
-	try:
-		fp = urllib.request.urlopen(link)
-	except:
-		print ("  Link is not valid, ignoring: " + link)
-		return ""
-	# An error in the connection can cause this, see this bug for more info: https://bugs.python.org/issue26499
-	# Hence, try 3 times before ignoring an ad
+# Helper function for __getAdAddress
+# Uses urllib to open the provided link
+# Attempts to retrieve extract the address from it
+# Error codes:
+# -1: urllib failed to get the address. Most likely the address is wrong
+# -2: Connection error, see this bug for more info: https://bugs.python.org/issue26499
+# -3: No location found in the html page
+# -4: Multiple different locations are found in the html page
+def __getAdAddress_direct(link):
+	try: fp = urllib.request.urlopen(link)
+	except: return -1
+
 	try: mybytes = fp.read()
-	except:
-		try: mybytes = fp.read() 
-		except:
-			try: mybytes = fp.read()
-			except:
-				return ''
+	except: return -2
+    
 	soup = BeautifulSoup(mybytes, 'html.parser')
 	
-	location = soup.find_all(class_=re.compile('locationContainer-*'))
-	if len(location) != 1:
-		print ("  Cannot find appropriate location")
+	location = soup.findAll(class_=re.compile('address-*'))
+	if len(location) < 1: return -3
+	elif len(location) > 1: # If multiple locations found, check if they are the same
+		first = location[0]
+		for l in location:
+			if l != first: return -4
+
+	return location[0].get_text().strip()
+
+# Attempts to get the address of a real estate advertisement
+# Input:
+#   * URL for the advertisement on Kijiji
+#   * Number of times we tried reaching this URL and failed before
+# Output: Adress of the advertisement
+def __getAdAddress(link, failuredConnection, failedProcessing):
+	if failuredConnection > 10: return ''
+	if failedProcessing > 3: return ''
+	address = __getAdAddress_direct(link)
+
+	if isinstance(address,int) and address < 0:
+		if address == -1: print("Link is not valid, ignoring: " + link)
+		if address == -2:
+			if failuredConnection == 0 and failedProcessing == 0: print()
+			print("  ~ Connection failure, waiting one minute before trying again")
+			time.sleep(60)
+			return __getAdAddress(link, failuredConnection + 1, failedProcessing)
+		if address == -3:
+			if failuredConnection == 0 and failedProcessing == 0: print()
+			print("  ~ Cannot find appropriate location, waiting one minute and trying again: ", link)
+			time.sleep(60)
+			return __getAdAddress(link, failuredConnection, failedProcessing + 1)
+		if address == -4: print("More than one different locations are available: ", link)
 		return ''
-	return list(location[0].children)[1].get_text().strip()
 
-# Takes a link for a real estate advertisement and attemps to get the
-# price offered
+	return address.replace('\n', ' ').replace('\r', '')
+
+# Attempts to get the address of a real estate advertisement
+# Input: URL for the advertisement on Kijiji
+# Output: Adress of the advertisement
 def getAdAddress(link):
-	address = __getAdAddress(link)
+    return __getAdAddress(link,0,0)
 
-	if address == '':
-		# Trying again helps in some cases
-		repeat = 0
-		while repeat < 5:
-			print("  Trying again...")
-			address = __getAdAddress(link)
-			if 'address' == '': repeat += 1
-			else: repeat = 6
-	
-	return address
-
-
+# Attemps to get the price offered on a real estate advertisement
+# Input: A `soup.find_all` result representing a real estate advertisement
+# Output: Price
+# Error codes:
+# -1: More than one price is available
+# -2: Price not available (need to contact the landlord privately for price)
+# -3: Price is not in the right format
 def getPriceOf(ad):
-    prices = ad.find_all('div',class_='price')
-    if len(prices) != 1: 
-        print ('  Error, more than one price for ad: ' + link)
-        return -1
-    else: price = prices[0].get_text().strip()
-    
-    # In some cases, landlord would not put the price on Kijiji, instead, asks interested people to contact
-    if price == 'Please Contact':
-        print ('  Price not found, ignoring entry')
-        return -1
-    
-    # Remove ads with prices that are not valid numbers
-    try:
-        priceFloat = float(price.replace('$','').replace(',',''))
-    except:
-        print ('  Price not in right format, ignoring this entry')
-        return -1
-    return price
+	prices = ad.find_all('div',class_='price')
+	if len(prices) != 1: return -1
+	else: price = prices[0].get_text().strip()
+
+	if price == 'Please Contact': return -2
+
+	# Remove ads with prices that are not valid numbers
+	try: priceFloat = float(price.replace('$','').replace(',',''))
+	except: return -3
+
+	return price
--- scraper.py
+++ scraper.py
@@ -1,23 +1,38 @@
 #!/usr/bin/env python3
 
-import urllib.request
+import urllib.request, time
 from bs4 import BeautifulSoup
 from MyLib.Scraping import getAdAddress, getPriceOf
 
+failedtrials = 0
 def getAds(url, processedLinks, db):
+	global failedtrials
+	print("  * URL: " + url)
+	time.sleep(5)
 	fp = urllib.request.urlopen(url)
 	mybytes = fp.read()
 	soup = BeautifulSoup(mybytes, 'html.parser')
 
 	ignored = 0
 	ads = soup.find_all(class_='search-item')
-	adCounter = 0
+	adCounter = 1
+	if len(ads) == 0:
+		failedtrials += 1
+		if failedtrials > 10:
+			print("  * Giving up on page after ten trials...")
+			return -1
+		print("  * Failed to read page, trying again in one minute")
+		time.sleep(60)
+		getAds(url, processedLinks,db)
+	else: failedtrials = 0
 	for ad in ads:
+		print("  - Ad %d/%d: " % (adCounter + ignored, len(ads)),end='')
+
 		# Get title and link
 		titles = ad.find_all(class_='title')
 		if len(titles) != 2:
 			ignored += 1
-			print ('  Error, there are no two tags with class "title" for this ad')
+			print ('Error, there are no two tags with class "title" for this ad')
 			continue
 		title = titles[0].find_all('a')[0].get_text().strip().replace('\n',' ').replace('\r',' ')
 		while '  ' in title: title = title.replace('  ',' ')
@@ -26,12 +41,19 @@ def getAds(url, processedLinks, db):
 		# Skip already processed entries
 		if link in processedLinks:
 			adCounter += 1
-			print('  Hit already saved ad: %d/%d'% (adCounter, len(ads)))
+			print('Hit already saved ad')
 			continue
 		
 		# Get price
 		price = getPriceOf(ad)
-		if price == -1:
+		if isinstance(price,int) and price < 0:
+			if price == -1: print ('Error, more than one price for ad')
+			elif price == -2: print ('Price not available, entry ignored')
+			elif price == -3: print ('Price not in right format, ignoring this entry')
+			else:
+				print("  * ERROR: getPriceOf gave an unexpected value")
+				print("    * URL: " + url)
+				print("    * Returned value: " + price)
 			ignored += 1
 			continue
 		
@@ -46,17 +68,19 @@ def getAds(url, processedLinks, db):
 		db.write(toWrite)
 		processedLinks.add(link)
 		adCounter += 1
-		print ('  Ad completed: %d/%d' % (adCounter, len(ads)))
+		print ('Ad completed')
 	db.flush()
 	return ignored
 
 # Get already processed ads
-db = open('apartmentInfo')
 processedLinks = set()
-for ad in db:
-	if not ad: continue
-	ad = ad.split(' --> ')
-	processedLinks.add(ad[2])
+try:
+	db = open('apartmentInfo')
+	for ad in db:
+		if not ad: continue
+		ad = ad.split(' --> ')
+		processedLinks.add(ad[2])
+except FileNotFoundError: ... # There is no apartmentInfo collected previouy
 
 # Process new ads
 db = open('apartmentInfo','a+')
@@ -65,6 +89,6 @@ baseLink = 'https://www.kijiji.ca/b-for-rent/gta-greater-toronto-area'
 nbOfPages = 100
 
 for page in range(nbOfPages):
-	print("Checking page: " + str(page + 1) + "/" + str(nbOfPages))
+	print("* Checking page: " + str(page + 1) + "/" + str(nbOfPages))
 	ignoredAds = getAds(baseLink + '/page-' + str(page) + '/c30349001l1700272?ad=offering', processedLinks, db)
-	print(str(ignoredAds) + " ignored ads")
+	if ignoredAds >= 0: print(str(ignoredAds) + " ignored ads")
